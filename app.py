cf7437d7e2a5725a06a9df71ab22da3c5c0d0868
Carlos Eduardo <eduardo.slayer17@gmail.com>
2023-05-26 12:42:48 -0300

    Finalização da chamada usando API do GoogleMaps

--- app.py
+++ app.py
@@ -1,48 +1,45 @@
-from flask import Flask, render_template, request
-import twitter
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import googlemaps
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'chave_secreta'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///presenca.db'
 
-aluno1 = ["Alecio Oliveira Gotti",'a1']
-aluno2 = ["Alyson Marques Gomes",'a2']
-aluno3 = ["Amanda de Almeida Passos",'a3']
-aluno4 = ["Ana Flavia Cezario de Oliveira",'a4']
-aluno5 = ["Ana Julia Candido Pereira",'a5']
+db = SQLAlchemy(app)
 
-@app.route("/", methods=["GET", "POST"])
-def main():
-    
-    if request.args.get('a1', None):
-        aluno1[1] = process_text(request.args['a1'])
-    
-    if request.args.get('a2', None):
-        aluno2[1] = process_text(request.args['a2'])
-        
-    if request.args.get('a3', None):
-        aluno3[1] = process_text(request.args['a3'])
-            
-    if request.args.get('a4', None):
-        aluno4[1] = process_text(request.args['a4'])
-            
-    if request.args.get('a5', None):
-        aluno5[1] = process_text(request.args['a5'])
-        
-    alunos = [aluno1,aluno2,aluno3,aluno4,aluno5]
+gmaps = googlemaps.Client(key='AIzaSyDSIZNte-JBAhTMCS_tKDyb2xZMGfGibl8')
 
-    return render_template('index.html', alunos=alunos)
+class Presenca(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    endereco = db.Column(db.String(200), nullable=False)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
 
-def process_text(text):
-    return "FOO" + text
-    
-class Twitter():
-    def __init__(self):
-        self.api = twitter.Api(consumer_key='B9Vr3WkqtarTKQBUbiTUJHtKa', consumer_secret='LfO7ZlIqUqbjyIKm6xSCyL1aQRstWWdPJTt4MwUIVj5TiRCHtM', access_token_key='1652051666621853697-XlUJyqmGddKWb7R0p0GTSxvsKZKc3G', access_token_secret='xigBLcL5l7Y1RHbJ3Roft6PON7uawNxVwRDiMCv0baLhn')
-    
-    def post_msg(self):
-        response = self.api.PostUpdate(app.aluno1[0]+'-'+app.aluno1[1]+'/n'+app.aluno2[0]+'-'+app.aluno2[1]+'/n'+app.aluno3[0]+'-'+app.aluno3[1]+'/n'+app.aluno4[0]+'-'+app.aluno4[1]+'/n'+app.aluno5[0]+'-'+app.aluno5[1]+'/n')
+@app.route('/', methods=['GET'])
+def exibir_formulario():
+    return render_template('form.html')
+
+@app.route('/presenca', methods=['POST'])
+def registrar_presenca():
+    nome = request.form['nome']
+    endereco = request.form['endereco']
+
+    geocode_result = gmaps.geocode(endereco)
+
+    if geocode_result:
+        metric = geocode_result[0]['formatted_address']
+        location = geocode_result[0]['geometry']['location']
+        latitude = location['lat']
+        longitude = location['lng']
+
+        presenca = Presenca(nome=nome, endereco=metric, latitude=latitude, longitude=longitude)
+        db.session.add(presenca)
+        db.session.commit()
+
+        return jsonify({'message': 'Presença registrada com sucesso!'}), 201
 
 
 if __name__ == '__main__':
-    app.run(debug= True)
-    t = Twitter()
-    t.post_msg()
+    app.run()
